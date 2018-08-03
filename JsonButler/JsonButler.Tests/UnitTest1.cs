667d27c6b75ab31f0b2f6e60a163d744366d1b30
Anurag D <anurag.devanapally@gmail.com>
2018-08-02 20:00:07 -0700

    Test project added. Serializer scaffolding implemented.

---
+++ JsonButler/JsonButler.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace JsonButler.Tests
+{
+
+    [TestClass]
+    public class JsonButlerTests
+    {
+        [TestMethod]
+        public void SerializeType_CustomType_Serialized () { }
+    }
+
+}
---
+++ JsonButler/JsonButler/CodeSerialization/ButlerSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace Andeart.JsonButler.CodeSerialization
+{
+
+    public class ButlerSerializer
+    {
+        public static void SerializeType<T> ()
+        {
+            SerializeType<T> (new ButlerSerializerSettings (typeof(T).Assembly));
+        }
+
+        public static void SerializeType<T> (ButlerSerializerSettings settings) { }
+
+        private static object CreatePrimitiveObject<T> ()
+        {
+            Type type = typeof(T);
+            return CreatePrimitiveObject (type);
+        }
+
+        private static object CreatePrimitiveObject (Type type)
+        {
+            if (type.IsValueType || type.IsPrimitive)
+            {
+                return Activator.CreateInstance (type);
+            }
+
+            return type.IsArray ? new object[0] : null;
+        }
+    }
+
+}
