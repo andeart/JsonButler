c5d609ca7f40f8c0a4fcf60dabb9719c938b972f
Anurag D <anurag.devanapally@gmail.com>
2019-02-25 20:59:00 -0800

    Update assembly names; Remove snk references

--- JsonButler/JsonButler.Tests/Properties/AssemblyInfo.cs
+++ JsonButler/JsonButler.Tests/Properties/AssemblyInfo.cs
@@ -2,17 +2,16 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle ("JsonButler.Tests")]
+[assembly: AssemblyTitle ("Andeart.JsonButler.Tests")]
 [assembly: AssemblyDescription ("JsonButler generates C# types and files from JSON data, and serializes types to JSON without explicit object instantiation.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Andeart")]
-[assembly: AssemblyProduct ("JsonButler.Tests")]
+[assembly: AssemblyProduct ("Andeart.JsonButler.Tests")]
 [assembly: AssemblyCopyright ("Copyright © 2019 ANURAG DEVANAPALLY")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 [assembly: ComVisible (false)]
 [assembly: Guid ("c4a42d52-7cec-41bc-badd-3b71c49babab")]
 
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("1.0.1.0")]
-[assembly: AssemblyFileVersion ("1.0.1.0")]  
+[assembly: AssemblyFileVersion ("1.0.1.0")]
