fb5a570d5bf80ef04d67a7bb7f52fa1bc3967eaa
andeart <andymations.ad@gmail.com>
2019-03-01 21:51:46 -0800

    Update assembly versions

--- JsonButler/JsonButler/Properties/AssemblyInfo.cs
+++ JsonButler/JsonButler/Properties/AssemblyInfo.cs
@@ -23,16 +23,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ("322eccb4-5e68-4051-ae27-43e962ea014f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.3.0")]
-[assembly: AssemblyFileVersion ("1.0.3.0")]
+[assembly: AssemblyVersion ("1.0.4.0")]
+[assembly: AssemblyFileVersion ("1.0.4.0")]
 [assembly: InternalsVisibleTo ("butler")]
